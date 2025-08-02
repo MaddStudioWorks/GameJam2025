95455969b23969184c276d967f42b721548434a4
unknown <67873431+Ackermiam@users.noreply.github.com>
2025-08-02 19:01:21 +0200

    update SoundManagement

--- src/sound-design/index.sound-design.ts
+++ src/sound-design/index.sound-design.ts
@@ -1,56 +1,63 @@
 import { Howl, Howler } from "howler";
-import theme from "/theme.mp3"
-import room2 from "/theme.mp3"
-import menu from "/menu.mp3"
-import room1 from "/room1.mp3"
+import BGMtheme from "/theme.mp3"
+import BGMmenu from "/menu.mp3"
+import BGMroom1 from "/room1.mp3"
+import BGMroom2 from "/room2.mp3"
+import BGMroom3 from "/room3.mp3"
+import BGMroom4 from "/room4.mp3"
+import BGMpattern1 from "/pattern1.mp3"
+import BGMpattern1wav from "/pattern1.wav"
+import BGMpattern1ogg from "/pattern.ogg"
+import BGMpattern2 from "/pattern2.mp3"
+import BGMpattern3 from "/pattern3.mp3"
+import BGMpattern4 from "/pattern4.mp3"
+
+export const bgm = {
+  theme: BGMtheme,
+  room1: BGMroom1,
+  room2: BGMroom2,
+  room3: BGMroom3,
+  room4: BGMroom4,
+  pattern1: BGMpattern1ogg,
+  pattern2: BGMpattern2,
+  pattern3: BGMpattern3,
+  pattern4: BGMpattern4,
+  pattern1wav: BGMpattern1wav,
+  menu: BGMmenu
+}
 
 export default class SoundManagement {
-  BGM: any;
-  SFX: any;
-  playBGMmusic: any;
-  playSFXsound: any;
+  bgm = bgm;
+  sfx: any;
+  playBGMmusic: Howl;
+  playSFXsound: Howl;
 
   constructor() {
-    this.BGM = {
-      'theme': theme,
-      hub: '',
-      'room1': room1,
-      'room2': room2,
-      room3: '',
-      room4: '',
-      'menu': menu
+    this.sfx = {
+      theme: BGMtheme,
+      room1: BGMroom1,
     };
 
-    this.SFX = {
-      'theme': theme,
-      hub: '',
-      'room1': room1,
-      'room2': room2,
-      room3: '',
-      room4: '',
-      'menu': menu
-    };
-
-    //this.playBGM('theme', true)
+    this.playBGM(this.bgm.pattern1, true)
   }
 
-  playBGM(music: string, loop: boolean): void {
+  playBGM(music: any, loop: boolean): void {
     this.playBGMmusic = new Howl({
-      src: [`${this.BGM[music]}`],
-      loop
+      src: [music],
+      loop,
+      volume: 0.5
     })
     this.playBGMmusic.play();
   }
 
-  changeMusicBGM(music: string, loop: boolean) {
-    this.playBGMmusic.src = `${this.BGM[music]}`;
-    this.playBGMmusic.loop = loop
-  }
-  playSFX(music: string): void {
+  playSFX(music: any): void {
     this.playSFXsound = new Howl({
-      src: [`${this.BGM[music]}`]
+      src: [music],
     })
-    this.playSFXsound.stop();
     this.playSFXsound.play();
+  }
+  
+  cleanAllMusic() {
+    Howler.unload();
   }
 }
